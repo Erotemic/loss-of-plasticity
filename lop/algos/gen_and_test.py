eb0868220cebb4930476ff51110cffddfb983b0a
joncrall <erotemic@gmail.com>
2023-09-11 14:07:16 -0400

    wip

--- lop/algos/gen_and_test.py
+++ lop/algos/gen_and_test.py
@@ -914,6 +914,8 @@ class TorchRunningStats:
     def mean(self):
         n = self.n
         total = self.raw_total
+        if n == 1:
+            return total
         return total / n
 
     def summarize(self):
@@ -936,8 +938,26 @@ class RecordActivationHook:
         activation = output.detach()
         if self.name not in self.meta.activation_cache:
             self.meta.activation_cache[self.name] = TorchRunningStats(device=activation.device)
-        runner = self.meta.activation_cache[self.name]
-        runner.update(activation)
+
+        try:
+            runner = self.meta.activation_cache[self.name]
+            runner.raw_total = activation
+            runner.n = 1
+            # runner.update(activation)
+        except Exception:
+            return
+            from watch.tasks.fusion.methods.channelwise_transformer import slice_to_agree
+            # Could do this as a hack...
+            a, b = slice_to_agree(activation, runner.raw_total)
+            # print(f'a.shape={a.shape}')
+            # print(f'b.shape={b.shape}')
+            # print(f'activation.shape       = {activation.shape}')
+            # print(f'runner.raw_total.shape = {runner.raw_total.shape}')
+
+            if np.prod(a.shape) > 0:
+                activation = a
+                runner.raw_total = b
+                runner.update(activation)
 
 
 def patched_trace_graph(net_copy, input_data):
